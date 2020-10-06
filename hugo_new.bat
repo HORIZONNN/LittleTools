ed0b7450ebba71152743cbfd4e092081b1fe59c7
Leon <1578644088@qq.com>
2020-10-06 11:41:33 +0800

    hugo bat

---
+++ hugo_new.bat
@@ -0,0 +1,3 @@
+d:
+cd D:\Dropbox\Code\HORIZONNN\mysite
+D:\PortableProgramFiles\Hugo\hugo.exe new \post\%1
---
+++ hugo_push.bat
@@ -0,0 +1,7 @@
+d:
+cd D:\Dropbox\Code\HORIZONNN\mysite
+D:\PortableProgramFiles\Hugo\hugo.exe
+cd public
+git add -A 
+git commit -m %1
+git push origin master
